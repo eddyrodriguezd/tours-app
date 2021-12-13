571aa1197f7291e25525a393e349c31d55ab5063
Alexander Marquez <alex18escorpio@gmail.com>
2021-12-13 10:29:31 -0500

    add banner empresas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-import Destinations from './views/Destinations';
+import logo from "./logo.svg";
+import "./App.css";
+import Destinations from "./views/Destinations";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
 
+// import Swiper core and required modules
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([EffectCoverflow, Pagination]);
 function App() {
   return (
     <div className="App">
-      <Destinations/>
+      <Destinations />
+
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="./assets/civa.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+            alt="hola"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+            alt="hola"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+            alt="hola"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-5.jpg"
+            alt="hola"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-6.jpg"
+            alt="hola"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-7.jpg"
+            alt="hola"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-8.jpg"
+            alt="hola"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-9.jpg"
+            alt="hola"
+          />
+        </SwiperSlide>
+      </Swiper>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
