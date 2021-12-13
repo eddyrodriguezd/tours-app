667de8a0bf3c5a84a0c0be0af2434e8d21fa06b9
Alexander Marquez <alex18escorpio@gmail.com>
2021-12-13 09:34:03 -0500

    bug fix destinationCard.js(add className), destinationCardGrid.js(add keys in map) and SearchBar.js(write autocomplete letter capitalize)

--- src/components/public/cards/DestinationCard.js
+++ src/components/public/cards/DestinationCard.js
@@ -1,14 +1,12 @@
-import './DestinationCard.css'
+import "./DestinationCard.css";
 
 export default function DestinationCard(props) {
-
-    return(
-        <div className="grid-element">
-            <img src={props.img} alt=""/>
-            <div class="card-text">
-                <h3>{props.name}</h3>
-            </div>
-        </div>
-    )
-
+  return (
+    <div className="grid-element">
+      <img src={props.img} alt="" />
+      <div className="card-text">
+        <h3>{props.name}</h3>
+      </div>
+    </div>
+  );
 }
--- src/components/public/cards/DestinationCardsGrid.js
+++ src/components/public/cards/DestinationCardsGrid.js
@@ -1,27 +1,37 @@
-import {Arequipa, Cajamarca, Chiclayo, Cuzco, Ica, Iquitos, Lima, Piura, Tarapoto} from './../../../assets/destinations';
-import DestinationCard from './DestinationCard';
-import './DestinationCard.css'
+import {
+  Arequipa,
+  Cajamarca,
+  Chiclayo,
+  Cuzco,
+  Ica,
+  Iquitos,
+  Lima,
+  Piura,
+  Tarapoto,
+} from "./../../../assets/destinations";
+import DestinationCard from "./DestinationCard";
+import "./DestinationCard.css";
 
 export default function DestinationCardsGrid(props) {
-    const destinations = [
-        { name: "Arequipa", image: Arequipa },
-        { name: "Cajamarca", image: Cajamarca },
-        { name: "Chiclayo", image: Chiclayo },
-        { name: "Cuzco", image: Cuzco },
-        { name: "Ica", image: Ica },
-        { name: "Iquitos", image: Iquitos },
-        { name: "Lima", image: Lima },
-        { name: "Piura", image: Piura },
-        { name: "Tarapoto", image: Tarapoto },
-      ];
+  const destinations = [
+    { name: "Arequipa", image: Arequipa },
+    { name: "Cajamarca", image: Cajamarca },
+    { name: "Chiclayo", image: Chiclayo },
+    { name: "Cuzco", image: Cuzco },
+    { name: "Ica", image: Ica },
+    { name: "Iquitos", image: Iquitos },
+    { name: "Lima", image: Lima },
+    { name: "Piura", image: Piura },
+    { name: "Tarapoto", image: Tarapoto },
+  ];
 
-    return (
-        <div className="grid">
-            {destinations
-            .filter((val) => val.name.toLowerCase().includes(props.destinations))
-            .map((item) =>
-                <DestinationCard name={item.name} img={item.image}/>
-            )}
-        </div>
-    )
+  return (
+    <div className="grid">
+      {destinations
+        .filter((val) => val.name.toLowerCase().includes(props.destinations))
+        .map((item, index) => (
+          <DestinationCard key={index} name={item.name} img={item.image} />
+        ))}
+    </div>
+  );
 }
