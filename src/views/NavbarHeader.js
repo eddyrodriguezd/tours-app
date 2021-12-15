5767dce42c4ca6d6af47551e2d347c1436ac0832
mr01robot <42098976+mr01robot@users.noreply.github.com>
2021-12-14 21:12:12 -0500

    footer,header and slider

---
+++ src/views/NavbarHeader.js
@@ -0,0 +1,16 @@
+import NavChildren from "../components/public/Header/NavChildren";
+
+
+export default function NavbarHeader(props){
+
+    return(
+        <>
+         <NavChildren/>
+         {props.children}
+        </>
+       
+      
+       
+      
+    );
+}
