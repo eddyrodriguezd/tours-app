11e37776ada67bda12fdda49463e3a323133285a
Eddy Rodriguez <ext_eddrodriguez@falabella.cl>
2022-01-13 11:14:09 -0500

    Moved destinations to mock folder

--- src/components/public/cards/DestinationCardsGrid.jsx
+++ src/components/public/cards/DestinationCardsGrid.jsx
@@ -1,44 +1,21 @@
-import {
-	Arequipa,
-	Cajamarca,
-	Chiclayo,
-	Cuzco,
-	Ica,
-	Iquitos,
-	Lima,
-	Piura,
-	Tarapoto,
-} from '../../../assets/destinations';
+import mock from '../../../mock/destinations/mockDestinations';
 import DestinationCard from './DestinationCard';
 import './DestinationCard.css';
 
-const DestinationCardsGrid = ({ destinationsFilter }) => {
-	const destinations = [
-		{ id: 1, name: 'Arequipa', image: Arequipa },
-		{ id: 2, name: 'Cajamarca', image: Cajamarca },
-		{ id: 3, name: 'Chiclayo', image: Chiclayo },
-		{ id: 4, name: 'Cuzco', image: Cuzco },
-		{ id: 5, name: 'Ica', image: Ica },
-		{ id: 6, name: 'Iquitos', image: Iquitos },
-		{ id: 7, name: 'Lima', image: Lima },
-		{ id: 8, name: 'Piura', image: Piura },
-		{ id: 9, name: 'Tarapoto', image: Tarapoto },
-	];
-	return (
-		<div className='grid'>
-			{destinations
-				.map((item) => (
-					<DestinationCard
-						key={item.id}
-						name={item.name}
-						img={item.image}
-					/>
-				))
-				.filter((val) => (
-					val.props.name.toLowerCase().includes(destinationsFilter)
-				))}
-		</div>
-	);
-};
+const DestinationCardsGrid = ({ destinationsFilter }) => (
+	<div className='grid'>
+		{mock.destinations
+			.map((item) => (
+				<DestinationCard
+					key={item.id}
+					name={item.name}
+					img={item.image}
+				/>
+			))
+			.filter((val) => (
+				val.props.name.toLowerCase().includes(destinationsFilter)
+			))}
+	</div>
+);
 
 export default DestinationCardsGrid;
---
+++ src/mock/destinations/mockDestinations.js
@@ -0,0 +1,23 @@
+import Arequipa from './assets/arequipa.jpg';
+import Cajamarca from './assets/cajamarca.jpg';
+import Chiclayo from './assets/chiclayo.jpg';
+import Cuzco from './assets/cuzco.jpg';
+import Ica from './assets/ica.jpg';
+import Iquitos from './assets/iquitos.jpg';
+import Lima from './assets/lima.jpg';
+import Piura from './assets/piura.jpg';
+import Tarapoto from './assets/tarapoto.jpg';
+
+const destinations = [
+    { id: 1, name: 'Arequipa', image: Arequipa },
+    { id: 2, name: 'Cajamarca', image: Cajamarca },
+    { id: 3, name: 'Chiclayo', image: Chiclayo },
+    { id: 4, name: 'Cuzco', image: Cuzco },
+    { id: 5, name: 'Ica', image: Ica },
+    { id: 6, name: 'Iquitos', image: Iquitos },
+    { id: 7, name: 'Lima', image: Lima },
+    { id: 8, name: 'Piura', image: Piura },
+    { id: 9, name: 'Tarapoto', image: Tarapoto },
+];
+
+export default { destinations };
