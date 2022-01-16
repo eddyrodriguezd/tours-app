75cfd24b9233ed6554a06a4bf020b8572d98dd31
Alexander Marquez <alex18escorpio@gmail.com>
2022-01-15 20:38:06 -0500

    ultimos cambios

--- src/components/private/cards/tours/TourCard.jsx
+++ src/components/private/cards/tours/TourCard.jsx
@@ -3,38 +3,37 @@ import './TourCard.css';
 
 const TourCard = ({ id, img, name, onEditClick, onDeleteClick }) => (
 	<div className='grid-wrapper'>
+		<div className='grid-row left'>
+			<img src={img} alt='' />
+		</div>
 
-        <div className='grid-row left'>
-		    <img src={img} alt='' />
-        </div>
-
-        <div className='grid-row right'>
-            <div className='card-name'>
-                <h3>{name}</h3>
-            </div>
-            <div className='card-buttons'>
-                <div className='card-button-column'>
-                    <div
-                        className='tour-card-edit-button'
-                        role='button'
-                        onClick={() => onEditClick(id)}
-                        onKeyDown={() => onEditClick(id)}
-                        tabIndex={0}>
-                        Editar
-                    </div>
-                </div>                
-                <div className='card-button-column'>
-                    <div
-                        className='tour-card-delete-button'
-                        role='button'
-                        onClick={() => onDeleteClick(id)}
-                        onKeyDown={() => onDeleteClick(id)}
-                        tabIndex={0}>
-                        Eliminar
-                    </div>
-                </div>
-            </div>
-        </div>
+		<div className='grid-row right'>
+			<div className='card-name'>
+				<h3>{name}</h3>
+			</div>
+			<div className='card-buttons'>
+				<div className='card-button-column'>
+					<div
+						className='tour-card-edit-button'
+						role='button'
+						onClick={() => onEditClick(id)}
+						onKeyDown={() => onEditClick(id)}
+						tabIndex={0}>
+						Editar
+					</div>
+				</div>
+				<div className='card-button-column'>
+					<div
+						className='tour-card-delete-button'
+						role='button'
+						onClick={() => onDeleteClick(id)}
+						onKeyDown={() => onDeleteClick(id)}
+						tabIndex={0}>
+						Eliminar
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 );
 
--- src/components/private/cards/tours/TourCardsGrid.jsx
+++ src/components/private/cards/tours/TourCardsGrid.jsx
@@ -2,15 +2,11 @@ import TourCard from './TourCard';
 import './TourCard.css';
 
 const TourCardsGrid = ({ tours }) => (
-    <div className='grid-tours'>
-        {tours.map((item) => (
-            <TourCard
-                key={item.id}
-                name={item.name}
-                img={item.image}
-            />
-        ))}
-    </div>
+	<div className='grid-tours'>
+		{tours.map((item) => (
+			<TourCard key={item.id} name={item.name} img={item.image} />
+		))}
+	</div>
 );
 
 export default TourCardsGrid;
